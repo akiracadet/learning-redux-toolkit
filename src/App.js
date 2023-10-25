a3d84dfc846b9310f974d5e24bc6329cf830058e
Lyndis <mavis.vermillion.ichi@gmail.com>
2023-10-25 23:18:40 +0800

    added posts project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import PostsList from './features/posts/PostsList'
+import AddPostForm from './features/posts/AddPostForm'
+
 export default function App() {
   return (
     <main className="App">
-
+      <AddPostForm />
+      <PostsList />
     </main>
   )
 }
---
+++ src/features/posts/PostAuthor.js
@@ -0,0 +1,11 @@
+import {useSelector} from 'react-redux'
+import {selectAllUsers} from '../users/usersSlice'
+
+export default function PostAuthor({userId}) {
+  const users = useSelector(selectAllUsers)
+  const author = users.find((users) => users.id === userId)
+
+  return (
+    <span>{author ? author.name : 'Unknown author'}</span>
+  )
+}
